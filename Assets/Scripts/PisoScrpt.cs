a7edfc3838edf3e57806811959de029e72bc9aa1
Nicodry20 <48178309@est.ort.edu.ar>
2024-06-03 15:57:53 -0300

    no se

---
+++ Assets/Scripts/PisoScrpt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PisoScrpt : MonoBehaviour
+{
+    public int resistencia;
+    public SphereScript esfera;
+
+    //Cuando un objeto colisione con este
+    //debe escribirse en la consola la plabra "Contacto"
+    private void OnCollisionEnter(Collision collision)
+    {
+        esfera = collision.gameObject.GetComponent<SphereScript>();
+        Debug.Log("Contacto con una esfera que pesa " + esfera.peso);
+    }
+}
