43181a747a71e0adac6595685f96ad6ec818889c
Nicodry20 <48178309@est.ort.edu.ar>
2024-06-06 09:27:41 -0300

    .

--- Assets/Scripts/Vehiculo.cs
+++ Assets/Scripts/Vehiculo.cs
@@ -7,12 +7,19 @@ public class Vehiculo : MonoBehaviour
     public int cantRuedas;
     public float precioAlquiler;
     public GameObject CartelPromo;
+    public Vehiculo[] vehiculos;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (cantRuedas >= 4)
+        {
+            if (CartelPromo != null)
+            {
+                CartelPromo.SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
